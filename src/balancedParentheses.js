72883c7670c1a09244e8caf4cfd8f890f0c8c550
Gregg Meluski <gmeluski@hotmail.com>
2016-06-15 12:52:35 -0700

    adding in balanced paren tests

--- src/balancedParentheses.js
+++ src/balancedParentheses.js
@@ -2,24 +2,53 @@
 
 /*
  * balancedParentheses
- * 
+ *
  * Takes an input string and returns true or false depending on if the string
  * has balanced parentheses.
- * 
+ *
  * Eg:
- * 
+ *
  *   input: '(x + y)'
  *   returns: true
- * 
+ *
  *   input: '(x + y'
  *   returns: false
- * 
+ *
  *   input: 'foo bar baz'
  *   returns: false
- * 
+ *
  *   input: ''
  *   returns: false
  */
 module.exports = ( input ) => {
+  var pairStack = [] ;
+  var currentChar;
 
+  // we never open a pair in the string
+  if (input.indexOf("(") === -1) {
+    return false;
+  }
+
+  // ok, so there is at least one opening paren, check the balance
+  for (var i = 0; i < input.length; i++) {
+    currentChar = input[i];
+
+    // if the qualifier we encouter is an opening paren, push it
+    if (currentChar === "(") {
+      pairStack.push(currentChar);
+    }
+
+
+    // if the qualifier is a closer, test to see if it is out of order
+    if (currentChar === ")" )  {
+      if (pairStack.length === 0) {
+        return false;
+      }
+
+      // if there is at least one opener on there, pop it
+      pairStack.pop()
+    }
+  }
+
+  return pairStack.length === 0;
 };
--- test/balancedParentheses.js
+++ test/balancedParentheses.js
@@ -17,3 +17,48 @@ test( 'BALANCED PARENTHESES: returns true for balanced string', t => {
     t.equal( result, true, 'result is correct' );
     t.end();
 } );
+
+test( 'BALANCED PARENTHESES: returns false for unbalanced string', t => {
+    const input = '(x + y';
+    const result = fpt.balancedParentheses( input );
+
+    t.equal( typeof result, 'boolean', 'generated a boolean result' );
+    t.equal( result, false, 'result is correct' );
+    t.end();
+} );
+
+test( 'BALANCED PARENTHESES: returns false for no parens', t => {
+    const input = 'foo bar baz';
+    const result = fpt.balancedParentheses( input );
+
+    t.equal( typeof result, 'boolean', 'generated a boolean result' );
+    t.equal( result, false, 'result is correct' );
+    t.end();
+} );
+
+test( 'BALANCED PARENTHESES: returns false for no length', t => {
+    const input = '';
+    const result = fpt.balancedParentheses( input );
+
+    t.equal( typeof result, 'boolean', 'generated a boolean result' );
+    t.equal( result, false, 'result is correct' );
+    t.end();
+} );
+
+test( 'BALANCED PARENTHESES: returns false for an inverted pair', t => {
+    const input = ')x + y(';;
+    const result = fpt.balancedParentheses( input );
+
+    t.equal( typeof result, 'boolean', 'generated a boolean result' );
+    t.equal( result, false, 'result is correct' );
+    t.end();
+} );
+
+test( 'BALANCED PARENTHESES: lets get wild', t => {
+    const input = '(x + y((hello)))(';
+    const result = fpt.balancedParentheses( input );
+
+    t.equal( typeof result, 'boolean', 'generated a boolean result' );
+    t.equal( result, false, 'result is correct' );
+    t.end();
+} );
