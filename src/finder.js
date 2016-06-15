b65185e8f0107e3778ef4e08ed642b349bc80858
Gregg Meluski <gmeluski@hotmail.com>
2016-06-15 13:25:09 -0700

    add in the shallow finder

--- src/finder.js
+++ src/finder.js
@@ -2,17 +2,29 @@
 
 /*
  * finder
- * 
+ *
  * Takes an input and a test function and returns any values in
  * the input that pass the test.
- * 
+ *
  * Eg:
- * 
+ *
  *   input: [ 'ant', 'baby', [ 'apple', 'banana', 'carrot' ], { foo: 'aardvark' }, 'allegory' ]
  *   test: value => /^a/i.test( value )
  *   returns: [ 'ant', 'allegory' ]
- * 
+ *
  */
 module.exports = ( input, test ) => {
+  var foundWords = [];
 
+  for (var i = 0; i < input.length; i++) {
+    var testObject = input[i];
+
+    if ((typeof testObject === 'string' || testObject instanceof String) &&
+      test(testObject)) {
+      foundWords.push(testObject);
+    }
+
+  }
+
+  return foundWords;
 };
